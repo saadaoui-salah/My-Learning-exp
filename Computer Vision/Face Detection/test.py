71624266f7160e7e8a76d8ea539d1aca8f8cead7
@saadaoui-salah <salahsaadaoui8@gmail.com>
2021-03-11 13:12:14 -0800

    update

--- Computer Vision/Face Detection/test.py
+++ Computer Vision/Face Detection/test.py
@@ -15,15 +15,15 @@ cv2.imshow('img',img)
 
 cv2.waitKey()
 
-#cam = cv2.VideoCapture(2)
-#if not (cam.isOpened()):
-#    print("couldn't")
-#cam.set(3, 640)
-#cam.set(4, 480)
-#cam.set(10,150)
-#while True:
-#    suc, img = cam.read()
-#    print(suc)
-#    cv2.imshow("camera", img)
-#    if cv2.waitKey(1) & 0xFF == ord('q'):
-#        break
+cam = cv2.VideoCapture(2)
+if not (cam.isOpened()):
+    print("couldn't")
+cam.set(3, 640)
+cam.set(4, 480)
+cam.set(10,150)
+while True:
+    suc, img = cam.read()
+    print(suc)
+    cv2.imshow("camera", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
